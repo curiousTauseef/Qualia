f5d065725b8b6cfdf0de5a8af36afc65b7bdb5a7
Kirk Roerig <mr.possoms@gmail.com>
2018-03-25 17:29:53 -0400

    Initial Commit

---
+++ qualia/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  qualia
+//
+//  Created by Kirk Roerig on 3/25/18.
+//  Copyright © 2018 Kirk Roerig. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
